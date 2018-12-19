af00a0672bd2bd174d1a59f9c7d66f0eee04b14d
Yuzhao <1371065684@qq.com>
2018-12-19 10:50:55 +0800

    Add a new class User

---
+++ checklist_Demo/checklist_Demo/Models/User.cs
@@ -0,0 +1,24 @@
+namespace checklist_Demo.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [StringLength(50)]
+        public string PassWord { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+    }
+}
